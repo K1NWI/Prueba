87acbcdb0a1d327430f5783577aa408ab221d1e2
Kinwi <jalvarez@itpfp.com>
2023-10-05 11:10:05 +0200

    decimo

--- Index.js
+++ Index.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
 
 app.get('/', function (req, res) {
-  res.sendFile('inicio.html');
-})
+  res.sendFile(path.join(__dirname, 'inicio.html'));
+});
 
-app.listen(3000)
+app.listen(3000);
