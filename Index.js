6396d386fdbb2efc6b700ab58e1d4ed76eab314c
Kinwi <jalvarez@itpfp.com>
2023-10-05 10:49:11 +0200

    Sexto

--- Index.js
+++ Index.js
@@ -1,11 +1,8 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const app = express()
 
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join('index.html'));
 });
 
-app.listen(3000, function() {
-    console.log('Servidor escuchando en el puerto 3000');
-});
+app.listen(3000)
