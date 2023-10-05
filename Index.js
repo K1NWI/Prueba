fe1b7ee9bab43d53238464e3040415bf3b0bb317
Kinwi <jalvarez@itpfp.com>
2023-10-05 10:51:34 +0200

    Septimo

--- Index.js
+++ Index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join('index.html'));
+  res.sendFile('index.html');
 });
 
 app.listen(3000)
